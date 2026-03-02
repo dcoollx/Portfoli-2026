83948cb968e54af3b50889f511318ec690e34e2b
dcoollx <dsqjr@msn.com>
2026-03-02 17:53:46 -0500

    added favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,6 +12,10 @@ const Layout: React.FC<LayoutProps> = ({ children }) => (
       <title>David Queen Jr - Software Engineer</title>
       <meta name="description" content="David Queen Jr's portfolio showcasing software engineering projects, skills, and experience." />
       <meta name="keywords" content="David Queen Jr, software engineer, portfolio, projects, skills, experience" />
+      <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+  <link rel="manifest" href="/favicon/site.webmanifest"></link>
     </Helmet>
     {children}
     </Container>
